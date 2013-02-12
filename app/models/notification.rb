8260d2b8cd33930948d7783e657beee2a33a750c
Andrey Kumanyaev <me@zzet.org>
2013-02-12 21:16:34 +0400

    Add notofication state to notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,27 @@
 class Notification < ActiveRecord::Base
   attr_accessible :event_id, :notification_state, :notified_at, :subscription_id
+
+  belongs_to :event
+  belongs_to :subscription
+  belongs_to :user, throuth: :subscription
+
+  state_machine :notification_state, initial: :new do
+    state :new
+    state :processing
+    state :delivered
+    state :failed
+
+    event :process do
+      transition [:new] => :processing
+    end
+
+    event :deliver do
+      transition [:processing] => :delivered
+    end
+
+    event :fail do
+      transition [:processing] => :failed
+    end
+  end
+
 end
