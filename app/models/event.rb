63ba66ea7fad7ce4713edffc880974d8def2afdf
Andrey Kumanyaev <me@zzet.org>
2013-02-12 23:20:51 +0400

    Add new relations with new events system

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,11 @@
-class Event::Base < ActiveRecord::Base
+class Event < ActiveRecord::Base
   attr_accessible :action, :data, :target_id, :target_type, :author_id
 
   belongs_to :author
   belongs_to :target, polymorphic: true
+  has_many :notifications, dependent: :destroy
+  has_many :subscriptions, through: :notifications
+  has_many :subscribers, through: :subscriptions, class_name: User
 
   validates :author,  presence: true
   validates :target,  presence: true
