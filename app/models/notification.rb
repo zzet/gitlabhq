9b1612409f9048d32942899ffb7bbed683c11644
Andrey Kumanyaev <me@zzet.org>
2013-02-12 20:24:06 +0400

    Add base of Notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,30 +1,3 @@
-class Notification
-  #
-  # Notification levels
-  #
-  N_DISABLED = 0
-  N_PARTICIPATING = 1
-  N_WATCH = 2
-
-  attr_accessor :user
-
-  def self.notification_levels
-    [N_DISABLED, N_PARTICIPATING, N_WATCH]
-  end
-
-  def initialize(user)
-    @user = user
-  end
-
-  def disabled?
-    user.notification_level == N_DISABLED
-  end
-
-  def participating?
-    user.notification_level == N_PARTICIPATING
-  end
-
-  def watch?
-    user.notification_level == N_WATCH
-  end
+class Notification < ActiveRecord::Base
+  attr_accessible :event_id, :notification_state, :notified_at, :subscription_id
 end
