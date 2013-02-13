9ec3a4cef99b3c9559cfddc6d89c0b7c01fd68b6
Andrew8xx8 <avk@8xx8.ru>
2013-02-13 18:21:30 +0400

    Event factory added

---
+++ spec/lib/gitlab/event/builder/users_project_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Gitlab::Event::Builder::UsersProject do
+  it "should respond that can build this data into action" do
+    pending "implement can_build?"
+  end
+
+  it "should build action from hash" do
+    pending "implement initialize"
+  end
+end
