f259cf20382d29643f9e262a3e8a7be28211adcd
Andrew8xx8 <avk@8xx8.ru>
2013-02-13 18:18:29 +0400

    Actionable added

---
+++ app/models/concerns/actionable.rb
@@ -0,0 +1,11 @@
+module Actionable
+  extend ActiveSupport::Concern
+
+  included do
+    extend Enumerize
+
+    enumerize :action, :in => [Event::Action.available_actions]
+
+    validates :action, presence: true
+  end
+end
