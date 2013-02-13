c57192bcd579b3297fd33d7e31e86108c56aad74
Andrew8xx8 <avk@8xx8.ru>
2013-02-13 17:18:29 +0400

    Classes renamed

--- app/models/event/subscription.rb
+++ app/models/event/subscription.rb
@@ -1,4 +1,4 @@
-class Subscription < ActiveRecord::Base
+class Event::Subscription < ActiveRecord::Base
   attr_accessible :action, :last_notified_at, :notification_interval, :target_id, :target_type, :user_id
 
   belongs_to :user
