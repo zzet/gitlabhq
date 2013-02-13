dbdfdbb0affa78b5f2a204010568cb083b58d2cd
Andrew8xx8 <avk@8xx8.ru>
2013-02-13 18:43:13 +0400

    Observers for activity and events added

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Gitlab
     # Activate observers that should always be running.
     config.active_record.observers = :old_activity_observer,
                                      :project_activity_cache_observer,
+                                     :activity_observer,
+                                     :event_observer,
                                      :issue_observer,
                                      :key_observer,
                                      :merge_request_observer,
