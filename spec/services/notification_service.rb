9afa0372e2ae511f0a40650aa10fb64076c185a2
Andrew8xx8 <avk@8xx8.ru>
2013-02-13 18:21:49 +0400

    Notification service added

---
+++ spec/services/notification_service.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe NotificationService do
+  it "should create notifications from event" do
+    pending "implement create_notifications"
+  end
+
+  it "should process notifications" do
+    pending "implement process_notifications"
+  end
+end
