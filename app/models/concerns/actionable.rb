46259e85e6a2a0450d0e0e7cbd19b097cc7864c4
Andrey Kumanyaev <me@zzet.org>
2013-02-14 18:15:37 +0400

    Fix errors

--- app/models/concerns/actionable.rb
+++ app/models/concerns/actionable.rb
@@ -4,7 +4,7 @@ module Actionable
   included do
     extend Enumerize
 
-    enumerize :action, :in => [Event::Action.available_actions]
+    enumerize :action, :in => Event::Action.available_actions
 
     validates :action, presence: true
   end
