61bd2ec2de89e7a03a68b7489ab2beb7cae26e6a
Andrey Kumanyaev <me@zzet.org>
2013-02-14 18:17:56 +0400

    Add Factori builders core

---
+++ lib/gitlab/event/builder/service.rb
@@ -0,0 +1,13 @@
+module Gitlab
+  module Event
+    module Builder
+      class Service < Gitlab::Event::Builder::Base
+        def can_build?(data)
+        end
+
+        def build(data)
+        end
+      end
+    end
+  end
+end
