59131cc2bfcf54ba2552a8fe08d82da027dbd4c1
Andrey Kumanyaev <me@zzet.org>
2013-02-14 18:17:56 +0400

    Add Factori builders core

--- lib/gitlab/event/factory.rb
+++ lib/gitlab/event/factory.rb
@@ -1,5 +1,23 @@
 module Gitlab
-  class Event::ActionFactory
+  class Event::Factory
+    class << self
+      def build(data)
+        events = []
 
+        Gitlab::Event::Builder::Base.descendants.each do |descendant|
+          events << descendant.build(data) if descendant.can_build?(data)
+        end
+
+        events
+      end
+
+      def create_events(data)
+        events = self.build(data)
+
+        events.each do |event|
+          event.save
+        end
+      end
+    end
   end
 end
