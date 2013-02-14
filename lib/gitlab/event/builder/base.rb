349a424da139d83c870de1fc17e68a94dfba296b
Andrey Kumanyaev <me@zzet.org>
2013-02-14 18:29:39 +0400

    Methods in buildes is self

--- lib/gitlab/event/builder/base.rb
+++ lib/gitlab/event/builder/base.rb
@@ -2,12 +2,14 @@ module Gitlab
   module Event
     module Builder
       class Base
-        def can_build?(data)
-          raise NotImplementedError
-        end
+        class << self
+          def can_build?(data)
+            raise NotImplementedError
+          end
 
-        def build(data)
-          raise NotImplementedError
+          def build(data)
+            raise NotImplementedError
+          end
         end
       end
     end
