60a5d842818c0559ce4a104f21157f3f10138e42
Andrey Kumanyaev <me@zzet.org>
2013-02-14 18:29:39 +0400

    Methods in buildes is self

--- lib/gitlab/event/builder/project.rb
+++ lib/gitlab/event/builder/project.rb
@@ -2,13 +2,15 @@ module Gitlab
   module Event
     module Builder
       class Project < Gitlab::Event::Builder::Base
-        def can_build?(data)
-          if data.is_a? Project
-          else
+        class << self
+          def can_build?(data)
+            if data.is_a? Project
+            else
+            end
           end
-        end
 
-        def build(data)
+          def build(data)
+          end
         end
       end
     end
