d3bbf989d215bb7da9f9fd7af01ccf5c1f7060cf
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:00:14 +0400

    Update notifications core

--- lib/gitlab/event/builder/issue.rb
+++ lib/gitlab/event/builder/issue.rb
@@ -2,8 +2,24 @@ module Gitlab
   module Event
     module Builder
       class Issue < Gitlab::Event::Builder::Base
+
+        @avaliable_action = [:created,
+                             :closed,
+                             :reopened,
+                             :deleted,
+                             :updated,
+                             :assigned,
+                             :reassigned,
+                             :commented
+                            ]
+
         class << self
-          def can_build?(data)
+          def can_build?(action, data)
+            known_action = known_action? @avaliable_action, action
+            # TODO Issue can be assigned to Milestone
+            # TODO Issue can be refference to Issue
+            known_target = data.is_a? ::Issue
+            known_target && known_action
           end
 
           def build(data)
--- lib/gitlab/event/builder/wiki.rb
+++ lib/gitlab/event/builder/wiki.rb
@@ -2,8 +2,19 @@ module Gitlab
   module Event
     module Builder
       class Wiki < Gitlab::Event::Builder::Base
+        @avaliable_action = [:created,
+                             :opend,
+                             :closed,
+                             :reopened,
+                             :deleted,
+                             :updated
+                            ]
+
         class << self
-          def can_build?(data)
+          def can_build?(action, data)
+            known_action = known_action? @avaliable_action, action
+            known_target = data.is_a? ::Wiki
+            known_target && known_action
           end
 
           def build(data)
