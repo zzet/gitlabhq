d1d736dd3f1beb156ca604566e018c732d0ef26b
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:02:30 +0400

    update builders with tests

--- spec/lib/gitlab/event/builder/milestone_spec.rb
+++ spec/lib/gitlab/event/builder/milestone_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Builder::Milestone do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+  before do
+    @milestone = create :milestone
+    @user = create :user
+    @data = {target: @milestone, user: @user, data: @milestone}
+    @action = "gitlab.created.milestone"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::Milestone.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::Milestone.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
+
 end
