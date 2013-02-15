1b1f324fda5eecc07f3e48034729e3129c85ee82
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:00:14 +0400

    Update notifications core

--- lib/gitlab/event/builder/web_hook.rb
+++ lib/gitlab/event/builder/web_hook.rb
@@ -2,8 +2,16 @@ module Gitlab
   module Event
     module Builder
       class WebHook < Gitlab::Event::Builder::Base
+        @avaliable_action = [:created,
+                             :deleted,
+                             :updated
+                            ]
+
         class << self
-          def can_build?(data)
+          def can_build?(action, data)
+            known_action = known_action? @avaliable_action, action
+            known_target = data.is_a? ::WebHook
+            known_target && known_action
           end
 
           def build(data)
