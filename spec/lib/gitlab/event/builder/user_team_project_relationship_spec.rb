e29293457549aa8e5bfb8328606f0aa4ef652da0
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:02:30 +0400

    update builders with tests

--- spec/lib/gitlab/event/builder/user_team_project_relationship_spec.rb
+++ spec/lib/gitlab/event/builder/user_team_project_relationship_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Builder::UserTeamProjectRelationship do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+  before do
+    @user_team_project_relationship = create :user_team_project_relationship
+    @user = create :user
+    @data = {target: @user_team_project_relationship, user: @user, data: @user_team_project_relationship}
+    @action = "gitlab.created.user_team_project_relationship"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::UserTeamProjectRelationship.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::UserTeamProjectRelationship.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
 end
--- spec/lib/gitlab/event/builder/user_team_spec.rb
+++ spec/lib/gitlab/event/builder/user_team_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Builder::UserTeam do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+  before do
+    @user_team = create :user_team
+    @user = create :user
+    @data = {target: @user_team, user: @user, data: @user_team}
+    @action = "gitlab.created.user_team"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::UserTeam.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::UserTeam.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
+
 end
