b0059eb2a5823c8ef27b1d076f3a330a0be35444
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:02:30 +0400

    update builders with tests

--- lib/gitlab/event/builder/merge_request.rb
+++ lib/gitlab/event/builder/merge_request.rb
@@ -3,26 +3,47 @@ module Gitlab
     module Builder
       class MergeRequest < Gitlab::Event::Builder::Base
 
-        @avaliable_action = [:created,
-                             :closed,
-                             :reopened,
-                             :deleted,
-                             :updated,
-                             :assigned,
-                             :reassigned,
-                             :commented,
-                             :merged
+        @avaliable_action = [:created,    # +
+                             :closed,     # +
+                             :reopened,   # +
+                             :deleted,    # +
+                             :updated,    # +
+                             :assigned,   # +
+                             :reassigned, # +
+                             :commented,  # -
+                             :merged      # -
                             ]
 
         class << self
           def can_build?(action, data)
             known_action = known_action? @avaliable_action, action
             # TODO Issue can be refference to MergeRequest
-            known_target = data.is_a? ::MergeRequest
+            known_target = data[:target].is_a? ::MergeRequest
             known_target && known_action
           end
 
-          def build(data)
+          def build(action, target, user, data)
+            meta = parse_action(action)
+            actions = []
+            actions << meta[:action]
+            case meta[:action]
+            when :created
+              actions << :assigned if target.assignee_changed?
+            when :updated
+              changes = target.changes
+
+              actions << :assigned if target.assignee_changed? && changes['assignee'].first.nil?
+              actions << :reassigned if target.assignee_changed? && changes['assignee'].first.present?
+
+              actions << :closed if target.is_being_closed?
+              actions << :reopened if target.is_being_reopened?
+            when :deleted
+            end
+
+            events = []
+            actions.each do |act|
+              events << ::Event.new(action: ::Event::Action.action_by_name(act), target: target, data: data.to_json, author: user)
+            end
           end
         end
       end
--- lib/gitlab/event/builder/namespace.rb
+++ lib/gitlab/event/builder/namespace.rb
@@ -2,20 +2,37 @@ module Gitlab
   module Event
     module Builder
       class Namespace < Gitlab::Event::Builder::Base
-        @avaliable_action = [:created,
-                             :deleted,
-                             :updated,
-                             :transfer
+        @avaliable_action = [:created, # +
+                             :deleted, # +
+                             :updated, # +
+                             :transfer # +
                             ]
 
         class << self
           def can_build?(action, data)
             known_action = known_action? @avaliable_action, action
-            known_target = data.is_a? ::Namespace
+            known_target = data[:target].is_a? ::Namespace
             known_target && known_action
           end
 
-          def build(data)
+          def build(action, target, user, data)
+            meta = parse_action(action)
+            actions = []
+            actions << meta[:action]
+            case meta[:action]
+            when :created
+            when :updated
+              changes = target.changes
+
+              # TODO puts here transfer action ckeck
+              actions << :transfer if target.owner != changes[:owner].first
+            when :deleted
+            end
+
+            events = []
+            actions.each do |act|
+              events << ::Event.new(action: ::Event::Action.action_by_name(act), target: target, data: data.to_json, author: user)
+            end
           end
         end
       end
