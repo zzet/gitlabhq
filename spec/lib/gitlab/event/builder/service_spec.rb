c9040ac94f22ff546ff029ad072dadd1e02e1966
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:20:29 +0400

    remove spaces

--- spec/lib/gitlab/event/builder/service_spec.rb
+++ spec/lib/gitlab/event/builder/service_spec.rb
@@ -15,5 +15,4 @@ describe Gitlab::Event::Builder::Service do
   it "should build events from hash" do
     @events = Gitlab::Event::Builder::Service.build(@action, @data[:target], @data[:user], @data[:data])
   end
-
 end
