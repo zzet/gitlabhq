4a96afd43041f4e9a090816aae97bf411801e9e6
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:04:14 +0400

    update global activity observer

--- app/observers/activity_observer.rb
+++ app/observers/activity_observer.rb
@@ -1,16 +1,18 @@
 class ActivityObserver < ActiveRecord::Observer
-  observe :key, :milestone, :namespace, :note, 
-    :project, :protected_branch, :service, 
-    :snippet, :user, :user_team, :user_team_project_relationship, 
+  observe :key, :milestone, :namespace, :note,
+    :project, :protected_branch, :service,
+    :snippet, :user, :user_team, :user_team_project_relationship,
     :user_team_user_relationship, :users_project, :web_hook, :wiki
 
-  def after_save(model)
-    # TODO. Remake with queue
-    Gitlab::Event::Factory.create_events(model)
+  def after_create(model)
+    # trigger action
+  end
+
+  def after_update(model)
+    # trigger action
   end
 
   def after_destroy(model)
-    # TODO. Remake with queue
-    Gitlab::Event::Factory.create_events(model)
+    # trigger action
   end
 end
