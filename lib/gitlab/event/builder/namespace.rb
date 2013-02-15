158f6f0140df17e4dda5815570acaddda6b4dff1
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:00:14 +0400

    Update notifications core

--- lib/gitlab/event/builder/namespace.rb
+++ lib/gitlab/event/builder/namespace.rb
@@ -2,8 +2,17 @@ module Gitlab
   module Event
     module Builder
       class Namespace < Gitlab::Event::Builder::Base
+        @avaliable_action = [:created,
+                             :deleted,
+                             :updated,
+                             :transfer
+                            ]
+
         class << self
-          def can_build?(data)
+          def can_build?(action, data)
+            known_action = known_action? @avaliable_action, action
+            known_target = data.is_a? ::Namespace
+            known_target && known_action
           end
 
           def build(data)
