f61539d797770b55058f7edde76815a480eef557
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:03:39 +0400

    Update mailing of notifications

--- app/models/event/subscription.rb
+++ app/models/event/subscription.rb
@@ -3,10 +3,16 @@ class Event::Subscription < ActiveRecord::Base
 
   attr_accessible :action, :last_notified_at, :notification_interval, :target_id, :target_type, :user_id
 
+  # Relations
   belongs_to :user
   belongs_to :target, polymorphic: true
   has_many :notifications, dependent: :destroy
 
+  # Validations
   validates :user, presence: true
   validates :target, presence: true
+
+  # Scopes
+  scope :on_event, ->(event) { where(action: event.action, target_id: event.target_id, target_type: event.target_type) }
+
 end
