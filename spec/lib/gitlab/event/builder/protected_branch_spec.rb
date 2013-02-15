f8f7c2b84cd649d67a027a002d0231229d5b59e9
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:02:30 +0400

    update builders with tests

--- spec/lib/gitlab/event/builder/protected_branch_spec.rb
+++ spec/lib/gitlab/event/builder/protected_branch_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Builder::ProtectedBranch do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+  before do
+    @protected_branch = create :protected_branch
+    @user = create :user
+    @data = {target: @protected_branch, user: @user, data: @protected_branch}
+    @action = "gitlab.created.protected_branch"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::ProtectedBranch.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::ProtectedBranch.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
 end
