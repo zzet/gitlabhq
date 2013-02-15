f9025cb3a9c74ad49686248215b75e55a53ae619
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:00:14 +0400

    Update notifications core

--- lib/gitlab/event/builder/user_team_project_relationship.rb
+++ lib/gitlab/event/builder/user_team_project_relationship.rb
@@ -2,8 +2,17 @@ module Gitlab
   module Event
     module Builder
       class UserTeamProjectRelationship < Gitlab::Event::Builder::Base
+        # Review
+        @avaliable_action = [:created,
+                             :deleted,
+                             :updated
+                            ]
+
         class << self
-          def can_build?(data)
+          def can_build?(action, data)
+            known_action = known_action? @avaliable_action, action
+            known_target = data.is_a? ::UserTeamProjectRelationship
+            known_target && known_action
           end
 
           def build(data)
