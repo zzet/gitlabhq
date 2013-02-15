ed909662170e25393fd979450b7d17f14639761d
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:03:39 +0400

    Update mailing of notifications

--- app/models/event/action.rb
+++ app/models/event/action.rb
@@ -1,24 +1,31 @@
 class Event::Action
   # General actions
-  CREATED   = 1
-  UPDATED   = 2
-  COMMENTED = 3
-  DELETED   = 4
-  ADDED     = 5
-  JOINED    = 6
-  LEFT      = 7
-  TRANSFER  = 8
+  CREATED    = 1
+  UPDATED    = 2
+  COMMENTED  = 3
+  DELETED    = 4
+  ADDED      = 5
+  JOINED     = 6
+  LEFT       = 7
+  TRANSFER   = 8
 
   # Git specific
-  PUSHED    = 9
-  CLONED    = 10
+  PUSHED     = 9
+  CLONED     = 10
 
   # Issuable
-  CLOSED    = 11
-  REOPENED  = 12
-  MERGED    = 13
+  CLOSED     = 11
+  REOPENED   = 12
+  MERGED     = 13
+  ASSIGNED   = 14
+  REASSIGNED = 15
 
   def self.available_actions
     self.constants
+  end
+
+  def self.action_to_s(action)
+    const = self.constants.find { |name| const_get(name) == action }
+    const.to_s.downcase!
   end
 end
