4c0fbf87b6a1297872b21cc595ace1eea9bbb8b5
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:02:30 +0400

    update builders with tests

--- spec/lib/gitlab/event/builder/service_spec.rb
+++ spec/lib/gitlab/event/builder/service_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Builder::Service do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+  before do
+    @service = create :service
+    @user = create :user
+    @data = {target: @service, user: @user, data: @service}
+    @action = "gitlab.created.service"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::Service.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::Service.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
 end
