998c5ec176d79c25cc3bac5df7c4d71c0871eac1
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:01:59 +0400

    Add notifications to models

--- app/models/user_team_project_relationship.rb
+++ app/models/user_team_project_relationship.rb
@@ -11,6 +11,8 @@
 #
 
 class UserTeamProjectRelationship < ActiveRecord::Base
+  include Gitlab::Event::Notifications
+
   attr_accessible :greatest_access, :project_id, :user_team_id
 
   belongs_to :user_team
