6d36d50193523cb600b2ca95a4ee76d18c2e7e1e
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:00:14 +0400

    Update notifications core

--- lib/gitlab/event/builder/merge_request.rb
+++ lib/gitlab/event/builder/merge_request.rb
@@ -2,8 +2,24 @@ module Gitlab
   module Event
     module Builder
       class MergeRequest < Gitlab::Event::Builder::Base
+
+        @avaliable_action = [:created,
+                             :closed,
+                             :reopened,
+                             :deleted,
+                             :updated,
+                             :assigned,
+                             :reassigned,
+                             :commented,
+                             :merged
+                            ]
+
         class << self
-          def can_build?(data)
+          def can_build?(action, data)
+            known_action = known_action? @avaliable_action, action
+            # TODO Issue can be refference to MergeRequest
+            known_target = data.is_a? ::MergeRequest
+            known_target && known_action
           end
 
           def build(data)
