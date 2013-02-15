b9171b53da9b808c199f0139d1396eece2fc6311
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:00:14 +0400

    Update notifications core

--- lib/gitlab/event/builder/base.rb
+++ lib/gitlab/event/builder/base.rb
@@ -10,6 +10,20 @@ module Gitlab
           def build(data)
             raise NotImplementedError
           end
+
+          def known_action?(known_action_list, action)
+            meta = parse_action(action)
+            known_action_list.includes? meta[:action]
+          end
+
+          def parse_action(action)
+            info = action.split "."
+            info.shift
+            {
+              action: info.shift.to_sym,
+              details: info
+            }
+          end
         end
       end
     end
