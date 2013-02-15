fdbd52eba2b3f8a57cd77a2974b87d38e40a03e0
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:02:30 +0400

    update builders with tests

--- spec/lib/gitlab/event/builder/users_project_spec.rb
+++ spec/lib/gitlab/event/builder/users_project_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Builder::UsersProject do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+  before do
+    @users_project = create :users_project
+    @user = create :user
+    @data = {target: @users_project, user: @user, data: @users_project}
+    @action = "gitlab.created.users_project"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::UsersProject.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::UsersProject.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
+
+
 end
--- spec/lib/gitlab/event/builder/wiki_spec.rb
+++ spec/lib/gitlab/event/builder/wiki_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Builder::Wiki do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+  before do
+    @wiki = create :wiki
+    @user = create :user
+    @data = {target: @wiki, user: @user, data: @wiki}
+    @action = "gitlab.created.wiki"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::Wiki.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::Wiki.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
 end
