49f80e151de5c6d9b6d25cce53ea25d3b13d6827
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:02:30 +0400

    update builders with tests

--- lib/gitlab/event/builder/base.rb
+++ lib/gitlab/event/builder/base.rb
@@ -3,22 +3,22 @@ module Gitlab
     module Builder
       class Base
         class << self
-          def can_build?(data)
+          def can_build?(action, data)
             raise NotImplementedError
           end
 
-          def build(data)
+          def build(action, target, user, data)
             raise NotImplementedError
           end
 
           def known_action?(known_action_list, action)
             meta = parse_action(action)
-            known_action_list.includes? meta[:action]
+            known_action_list.include? meta[:action]
           end
 
           def parse_action(action)
             info = action.split "."
-            info.shift
+            info.shift # Shift "gitlab"
             {
               action: info.shift.to_sym,
               details: info
--- spec/lib/gitlab/event/builder/snippet_spec.rb
+++ spec/lib/gitlab/event/builder/snippet_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Builder::Snippet do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+  before do
+    @snippet = create :snippet
+    @user = create :user
+    @data = {target: @snippet, user: @user, data: @snippet}
+    @action = "gitlab.created.snippet"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::Snippet.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::Snippet.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
 end
