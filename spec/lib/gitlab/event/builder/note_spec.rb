43e415cce91d3d0c441a509b02996a08822c7d1d
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:02:30 +0400

    update builders with tests

--- spec/lib/gitlab/event/builder/note_spec.rb
+++ spec/lib/gitlab/event/builder/note_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
-describe Gitlab::Event::Builder::Namespace do
-  it "should respond that can build this data into action" do
-    pending "implement can_build?"
+describe Gitlab::Event::Builder::Note do
+  before do
+    @note = create :note
+    @user = create :user
+    @data = {target: @note, user: @user, data: @note}
+    @action = "gitlab.created.note"
   end
 
-  it "should build action from hash" do
-    pending "implement initialize"
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::Note.can_build?(@action, @data).should be_true
   end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::Note.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+
 end
