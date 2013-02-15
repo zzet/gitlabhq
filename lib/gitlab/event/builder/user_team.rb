494132eba742e3b9dc9e699627c51c36bbfa2b89
Andrey Kumanyaev <me@zzet.org>
2013-02-15 04:00:14 +0400

    Update notifications core

--- lib/gitlab/event/builder/user_team.rb
+++ lib/gitlab/event/builder/user_team.rb
@@ -2,8 +2,19 @@ module Gitlab
   module Event
     module Builder
       class UserTeam < Gitlab::Event::Builder::Base
+        @avaliable_action = [:created,
+                             :deleted,
+                             :updated,
+                             :assigned,
+                             :reassigned,
+                             :transfer
+                            ]
+
         class << self
-          def can_build?(data)
+          def can_build?(action, data)
+            known_action = known_action? @avaliable_action, action
+            known_target = data.is_a? ::UserTeam
+            known_target && known_action
           end
 
           def build(data)
