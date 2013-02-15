f72eced1cb6c41317bee2a21c81c80299fdd0e30
Andrey Kumanyaev <me@zzet.org>
2013-02-16 01:20:29 +0400

    remove spaces

--- spec/lib/gitlab/event/builder/user_team_user_relationship_spec.rb
+++ spec/lib/gitlab/event/builder/user_team_user_relationship_spec.rb
@@ -15,6 +15,4 @@ describe Gitlab::Event::Builder::UserTeamUserRelationship do
   it "should build events from hash" do
     @events = Gitlab::Event::Builder::UserTeamUserRelationship.build(@action, @data[:target], @data[:user], @data[:data])
   end
-
-
 end
