30d59c65240281170e49d629a171ba04e4c0c483
Andrey Kumanyaev <me@zzet.org>
2013-02-17 17:03:01 +0400

    Fix bug in notification generator

--- lib/gitlab/event/notifications.rb
+++ lib/gitlab/event/notifications.rb
@@ -5,11 +5,11 @@ module Gitlab
 
       def self.trigger(action, target, user = nil, data = nil, detailed_event = "")
         data = target if data.blank?
-        target = target.class.name unless target.is_a? String
+        target_name = target.class.name unless target.is_a? String
         action = action.to_s unless action.is_a? String
         user = current_user if user.blank?
 
-        event = "gitlab.#{action}.#{target}".downcase
+        event = "gitlab.#{action}.#{target_name}".downcase
         event << ".#{detailed_event}" unless detailed_event.blank?
 
         ActiveSupport::Notifications.instrument event, {target: target, user: user, data: data}
