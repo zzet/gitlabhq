270369fbc607698cf1b470a50d9fd9d258d1ff44
Andrey Kumanyaev <me@zzet.org>
2013-02-17 16:55:23 +0400

    Update EventNotificationWorker (develop more readable observe)

--- app/workers/event_notification_worker.rb
+++ app/workers/event_notification_worker.rb
@@ -1,6 +1,7 @@
 class EventNotificationWorker
   def call(name, started, finished, unique_id, data)
-    Rails.logger.info "Triggered action" << name
+    Rails.logger.info "Triggered action: " << name
+
     Gitlab::Event::Factory.create_events(name, data)
   end
 end
