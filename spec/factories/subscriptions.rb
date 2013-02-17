81d549770a09d5eef79807b5a053c24da2d883ee
Andrey Kumanyaev <me@zzet.org>
2013-02-17 17:03:54 +0400

    Fix Subscription factory

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :subscription do
+  factory :subscription, class: Event::Subscription do
     user
     action "MyString"
     target_id 1
