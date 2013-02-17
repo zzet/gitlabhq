30a49dcb4ed64689586eb106d23aa1ea6e8b256d
Andrey Kumanyaev <me@zzet.org>
2013-02-17 18:51:26 +0400

    Avaliable actions must bu Fixnum list

--- spec/models/event/action.rb
+++ spec/models/event/action.rb
@@ -4,5 +4,8 @@ describe Event::Action do
   describe "should return array of available actions actions" do
     action_types = Event::Action.available_actions
     action_types.should be_kind_of Array
+    action_types.each do |action|
+      action.should be_kind_of Fixnum
+    end
   end
 end
