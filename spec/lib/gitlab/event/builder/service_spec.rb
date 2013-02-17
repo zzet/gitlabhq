bbf142d354fcec479911f86b2b634b60ca09c7cd
Andrey Kumanyaev <me@zzet.org>
2013-02-17 17:02:25 +0400

    Fix factory work

--- spec/lib/gitlab/event/builder/service_spec.rb
+++ spec/lib/gitlab/event/builder/service_spec.rb
@@ -9,7 +9,7 @@ describe Gitlab::Event::Builder::Service do
   end
 
   it "should respond that can build this data into action" do
-    Gitlab::Event::Builder::Service.can_build?(@action, @data).should be_true
+    Gitlab::Event::Builder::Service.can_build?(@action, @data[:target]).should be_true
   end
 
   it "should build events from hash" do
