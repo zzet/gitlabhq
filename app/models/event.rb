8edfd4d434cb28d5772a1bc64048f277bd16aadc
Andrey Kumanyaev <me@zzet.org>
2013-02-17 16:51:44 +0400

    Update Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,10 @@
 class Event < ActiveRecord::Base
   include Actionable
 
-  attr_accessible :action, :data, :target_id, :target_type, :author_id
+  attr_accessible :action, :data, :target_id, :target_type, :author_id, :target, :author
 
-  belongs_to :author
+
+  belongs_to :author, class_name: User
   belongs_to :target, polymorphic: true
 
   has_many :notifications,  dependent: :destroy,     class_name: Event::Subscription::Notification
@@ -12,4 +13,6 @@ class Event < ActiveRecord::Base
 
   validates :author,  presence: true
   validates :target,  presence: true
+
+  scope :with_target, ->(target) { where(target_id: target, target_type: target.class.name) }
 end
