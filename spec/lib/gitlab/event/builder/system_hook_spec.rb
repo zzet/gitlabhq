8938a45c66a8a682596cdee739d74a4d47b93e84
Andrey Kumanyaev <me@zzet.org>
2013-02-17 16:59:43 +0400

    Rename Namespace to Group and WebHook to ProjectHook and SystemHook

---
+++ spec/lib/gitlab/event/builder/system_hook_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Gitlab::Event::Builder::SystemHook do
+  before do
+    ActiveRecord::Base.observers.disable :all
+    SystemHook.observers.enable :activity_observer
+
+    @system_hook = create :system_hook
+    @user = create :user
+    @data = {target: @system_hook, user: @user, data: @system_hook}
+    @action = "gitlab.created.system_hook"
+  end
+
+  it "should respond that can build this data into action" do
+    Gitlab::Event::Builder::SystemHook.can_build?(@action, @data[:target]).should be_true
+  end
+
+  it "should build events from hash" do
+    @events = Gitlab::Event::Builder::SystemHook.build(@action, @data[:target], @data[:user], @data[:data])
+  end
+end
