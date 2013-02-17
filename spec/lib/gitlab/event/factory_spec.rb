9af96f1371208ced427a5463aed42912f5d062ca
Andrey Kumanyaev <me@zzet.org>
2013-02-17 17:07:57 +0400

    add pending test to factory

--- spec/lib/gitlab/event/factory_spec.rb
+++ spec/lib/gitlab/event/factory_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe Gitlab::Event::Factory do
-  it "should create event from hash" do
+  it "should build unsaved events on action" do
+    pending "implement create_event"
+  end
+
+  it "should create events from action" do
     pending "implement create_event"
   end
 end
