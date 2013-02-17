600dfde9afd9814811b42e047af2383bd0fe8c10
Andrey Kumanyaev <me@zzet.org>
2013-02-17 17:03:35 +0400

    fix Notification factory

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,10 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :notification do
-    event_id 1
-    subscription_id 1
-    notification_state "MyString"
+  factory :notification, class: Event::Subscription::Notification do
+    event
+    subscription
+    notification_state "new"
     notified_at "2013-02-12 16:19:47"
   end
 end
