310aa8e833528a85c47361e01fa5e6de4125f826
Andrey Kumanyaev <me@zzet.org>
2013-02-17 17:00:21 +0400

    Rename Event factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :event_basis, :class => 'Event::Base' do
-    user
+  factory :event, :class => 'Event' do
+    author
     action "Action"
     target_id 1
     target_type "MyString"
