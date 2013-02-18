fa25a398b937f7f5a0775e6c41172fdae4e132fe
Andrey Kumanyaev <me@zzet.org>
2013-02-19 03:01:51 +0400

    Separate Event Actions

---
+++ lib/gitlab/event/action/snippet.rb
@@ -0,0 +1,23 @@
+module Gitlab
+  module Event
+    module Action
+      module Snippet
+        extend ActiveSupport::Concern
+        include Gitlab::Event::Action::Base
+
+        included do
+          class << self
+            def avaliable_actions(user = nil, source = nil)
+              base_actions = [
+                :created,
+                :deleted,
+                :updated
+              ]
+            end
+          end
+        end
+
+      end
+    end
+  end
+end
