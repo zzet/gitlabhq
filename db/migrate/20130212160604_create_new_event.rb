4caa4081ada324e3f713e553d7426658b9f0319f
Andrey Kumanyaev <me@zzet.org>
2013-02-19 03:00:02 +0400

    Rename target to source and add source and source_category into subscrption

--- db/migrate/20130212160604_create_new_event.rb
+++ db/migrate/20130212160604_create_new_event.rb
@@ -2,9 +2,9 @@ class CreateNewEvent < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.integer :author_id
-      t.string :action
-      t.integer :target_id
-      t.string :target_type
+      t.integer :action
+      t.integer :source_id
+      t.string :source_type
       t.text :data
 
       t.timestamps
