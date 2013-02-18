a4f04b4ef7243c91767ac345af9f14c178156235
Andrey Kumanyaev <me@zzet.org>
2013-02-19 03:00:02 +0400

    Rename target to source and add source and source_category into subscrption

--- spec/lib/gitlab/event/builder/users_project_spec.rb
+++ spec/lib/gitlab/event/builder/users_project_spec.rb
@@ -4,15 +4,15 @@ describe Gitlab::Event::Builder::UsersProject do
   before do
     @users_project = create :users_project
     @user = create :user
-    @data = {target: @users_project, user: @user, data: @users_project}
+    @data = {source: @users_project, user: @user, data: @users_project}
     @action = "gitlab.created.users_project"
   end
 
   it "should respond that can build this data into action" do
-    Gitlab::Event::Builder::UsersProject.can_build?(@action, @data[:target]).should be_true
+    Gitlab::Event::Builder::UsersProject.can_build?(@action, @data[:source]).should be_true
   end
 
   it "should build events from hash" do
-    @events = Gitlab::Event::Builder::UsersProject.build(@action, @data[:target], @data[:user], @data[:data])
+    @events = Gitlab::Event::Builder::UsersProject.build(@action, @data[:source], @data[:user], @data[:data])
   end
 end
