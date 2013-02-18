0788a5014fcb8d2a0486ddd30a589a3b8e8e176b
Andrey Kumanyaev <me@zzet.org>
2013-02-19 03:01:51 +0400

    Separate Event Actions

--- lib/gitlab/event/builder/base.rb
+++ lib/gitlab/event/builder/base.rb
@@ -10,20 +10,6 @@ module Gitlab
           def build(action, target, user, data)
             raise NotImplementedError
           end
-
-          def known_action?(known_action_list, action)
-            meta = parse_action(action)
-            known_action_list.include? meta[:action]
-          end
-
-          def parse_action(action)
-            info = action.split "."
-            info.shift # Shift "gitlab"
-            {
-              action: info.shift.to_sym,
-              details: info
-            }
-          end
         end
       end
     end
