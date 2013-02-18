a912c666cd08606680e0bae4618035c832274f55
Andrey Kumanyaev <me@zzet.org>
2013-02-19 03:00:02 +0400

    Rename target to source and add source and source_category into subscrption

--- spec/lib/gitlab/event/builder/milestone_spec.rb
+++ spec/lib/gitlab/event/builder/milestone_spec.rb
@@ -4,15 +4,15 @@ describe Gitlab::Event::Builder::Milestone do
   before do
     @milestone = create :milestone
     @user = create :user
-    @data = {target: @milestone, user: @user, data: @milestone}
+    @data = {source: @milestone, user: @user, data: @milestone}
     @action = "gitlab.created.milestone"
   end
 
   it "should respond that can build this data into action" do
-    Gitlab::Event::Builder::Milestone.can_build?(@action, @data[:target]).should be_true
+    Gitlab::Event::Builder::Milestone.can_build?(@action, @data[:source]).should be_true
   end
 
   it "should build events from hash" do
-    @events = Gitlab::Event::Builder::Milestone.build(@action, @data[:target], @data[:user], @data[:data])
+    @events = Gitlab::Event::Builder::Milestone.build(@action, @data[:source], @data[:user], @data[:data])
   end
 end
--- spec/lib/gitlab/event/builder/snippet_spec.rb
+++ spec/lib/gitlab/event/builder/snippet_spec.rb
@@ -4,15 +4,15 @@ describe Gitlab::Event::Builder::Snippet do
   before do
     @snippet = create :snippet
     @user = create :user
-    @data = {target: @snippet, user: @user, data: @snippet}
+    @data = {source: @snippet, user: @user, data: @snippet}
     @action = "gitlab.created.snippet"
   end
 
   it "should respond that can build this data into action" do
-    Gitlab::Event::Builder::Snippet.can_build?(@action, @data[:target]).should be_true
+    Gitlab::Event::Builder::Snippet.can_build?(@action, @data[:source]).should be_true
   end
 
   it "should build events from hash" do
-    @events = Gitlab::Event::Builder::Snippet.build(@action, @data[:target], @data[:user], @data[:data])
+    @events = Gitlab::Event::Builder::Snippet.build(@action, @data[:source], @data[:user], @data[:data])
   end
 end
