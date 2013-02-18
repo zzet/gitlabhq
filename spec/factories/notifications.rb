db5c5feda79f2f1e21132b22d5f252d6d4b2f000
Andrey Kumanyaev <me@zzet.org>
2013-02-19 03:04:02 +0400

    fix tests

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :notification, class: Event::Subscription::Notification do
     event
     subscription
+    target
     notification_state "new"
     notified_at "2013-02-12 16:19:47"
   end
