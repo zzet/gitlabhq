4934d28a8d9d3badb3d34b41015de6130e384b61
Andrey Kumanyaev <me@zzet.org>
2013-02-20 03:00:36 +0400

    Add related events and more more more tests

--- lib/gitlab/event/builder/group.rb
+++ lib/gitlab/event/builder/group.rb
@@ -7,27 +7,58 @@ module Gitlab
         class << self
           def can_build?(action, data)
             known_action = known_action? action
-            known_source = data.is_a? ::Group
+            known_sources = [::Group, ::Project]
+            known_source = known_sources.include? data.class
             known_source && known_action
           end
 
           def build(action, source, user, data)
             meta = parse_action(action)
-            actions = []
-            actions << meta[:action]
-            case meta[:action]
-            when :created
-            when :updated
-              changes = source.changes
 
-              actions << :transfer if source.owner_id_changed? && source.owner_id != changes[:owner_id].first
-            when :deleted
+            target = source
+
+            actions = []
+
+            case source
+            when ::Group
+              actions << meta[:action]
+
+              case meta[:action]
+              when :created
+              when :updated
+                changes = source.changes
+
+                actions << :transfer if source.owner_id_changed? && source.owner_id != changes[:owner_id].first
+              when :deleted
+              end
+
+            when ::Project
+              # TODO. refactoring
+              target = source.group if source.group.present?
+
+              case meta[:action]
+              when :created
+                actions << :added if source.group == target
+              when :updated
+                changes = source.changes
+
+                # TODO. refactor
+                actions << :added if source.namespace_id_changed? && source.namespace_id != changes[:namespace_id].first && source.namespace == target
+                actions << :transfer if source.namespace_id_changed? && source.namespace_id != changes[:namespace_id].first && Group.find_by_id(changes["namespace_id"]).present?
+              when :deleted
+                actions << :deleted
+              end
             end
 
             events = []
+
             actions.each do |act|
-              events << ::Event.new(action: ::Event::Action.action_by_name(act), source: source, data: data.to_json, author: user)
+              events << ::Event.new(action: ::Event::Action.action_by_name(act),
+                                    source_id: source.id, source_type: source.class.name,
+                                    target_id: target.id, target_type: target.class.name,
+                                    data: data.to_json, author: user)
             end
+
             events
           end
         end
