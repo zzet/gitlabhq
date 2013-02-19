0d11a8174bc65759b77fe12c5d19ea350c2444ff
Andrey Kumanyaev <me@zzet.org>
2013-02-20 02:58:14 +0400

    Event has target

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,14 @@
 class Event < ActiveRecord::Base
   include Actionable
 
-  attr_accessible :action, :data, :source_id, :source_type, :author_id, :source, :author
+  attr_accessible :action, :data,
+                  :source_id, :source_type, :source,
+                  :target_id, :target_type, :target,
+                  :author_id, :author
 
 
   belongs_to :author, class_name: User
+  belongs_to :target, polymorphic: true
   belongs_to :source, polymorphic: true
 
   has_many :notifications,  dependent: :destroy,     class_name: Event::Subscription::Notification
@@ -15,4 +19,6 @@ class Event < ActiveRecord::Base
   validates :source,  presence: true
 
   scope :with_source, ->(source) { where(source_id: source, source_type: source.class.name) }
+  scope :recent, -> { order("created_at DESC") }
+  scope :with_target, ->(target) { where(target_id: target, target_type: target.class.name) }
 end
