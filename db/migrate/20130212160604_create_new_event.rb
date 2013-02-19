5c849e1ce63c086e5ec2a4f66277a981c29f123b
Andrey Kumanyaev <me@zzet.org>
2013-02-20 02:58:14 +0400

    Event has target

--- db/migrate/20130212160604_create_new_event.rb
+++ db/migrate/20130212160604_create_new_event.rb
@@ -3,8 +3,13 @@ class CreateNewEvent < ActiveRecord::Migration
     create_table :events do |t|
       t.integer :author_id
       t.integer :action
+
       t.integer :source_id
       t.string :source_type
+
+      t.integer :target_id
+      t.string :target_type
+
       t.text :data
 
       t.timestamps
