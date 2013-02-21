8a4326e8c6ec3333ae4af4abf14c739155c9e389
Andrey Kumanyaev <me@zzet.org>
2013-02-21 16:22:30 +0400

    update subsctiption logic

--- lib/gitlab/event/subscriptions/group.rb
+++ lib/gitlab/event/subscriptions/group.rb
@@ -5,6 +5,13 @@ module Gitlab
         include Gitlab::Event::Action::Group
 
         class << self
+          def can_subscribe?(user, action, target, source)
+            if target.is_a? ::Group
+              return true if user.is_admin?
+              return true if target.owner == user
+              return false
+            end
+          end
         end
 
       end
