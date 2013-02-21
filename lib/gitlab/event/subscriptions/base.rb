b8cb794e3d5916d76986d9775e0a63b87d785888
Andrey Kumanyaev <me@zzet.org>
2013-02-21 16:22:30 +0400

    update subsctiption logic

--- lib/gitlab/event/subscriptions/base.rb
+++ lib/gitlab/event/subscriptions/base.rb
@@ -4,23 +4,11 @@ module Gitlab
       class Base
 
         class << self
-          def subscribe(user, action, source, target)
-            action = ::Event::Action.action_by_name(action)
-            subscription = nil
-            case source.class.name
-            when "Symbol"
-              subscription = ::Event::Subscription.new(user: user, action: action, source_category: source)
-            else
-              unless source.is_a? Class
-                subscription = ::Event::Subscription.new(user: user, action: action, source: source)
-              else
-                subscription = ::Event::Subscription.new(user: user, action: action, source_category: source.name.downcase.to_sym)
-              end
-            end
-            subscription.target = target unless (target.is_a? Class)
-            subscription.save
+          def can_subscribe?(user, action, target, source)
+            return true
           end
         end
+
       end
     end
   end
