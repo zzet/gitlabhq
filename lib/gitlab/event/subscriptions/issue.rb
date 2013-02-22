c7b1bec5568e071407ece4e38801134688172560
Andrew8xx8 <avk@8xx8.ru>
2013-02-22 19:33:16 +0400

    Aaction now symbols

--- lib/gitlab/event/subscriptions/issue.rb
+++ lib/gitlab/event/subscriptions/issue.rb
@@ -2,8 +2,6 @@ module Gitlab
   module Event
     module Subscriptions
       class Issue < Gitlab::Event::Subscriptions::Base
-        include Gitlab::Event::Action::Issue
-
         class << self
           def can_subscribe?(user, action, target, source)
             return true
--- lib/gitlab/event/subscriptions/system_hook.rb
+++ lib/gitlab/event/subscriptions/system_hook.rb
@@ -2,8 +2,6 @@ module Gitlab
   module Event
     module Subscriptions
       class SystemHook < Gitlab::Event::Subscriptions::Base
-        include Gitlab::Event::Action::SystemHook
-
         class << self
         end
 
