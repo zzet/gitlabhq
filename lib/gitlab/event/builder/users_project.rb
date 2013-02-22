bb0ba939914c9484c6ffd531a899e7078a2fa048
Andrew8xx8 <avk@8xx8.ru>
2013-02-22 19:33:16 +0400

    Aaction now symbols

--- lib/gitlab/event/builder/users_project.rb
+++ lib/gitlab/event/builder/users_project.rb
@@ -2,11 +2,9 @@ module Gitlab
   module Event
     module Builder
       class UsersProject < Gitlab::Event::Builder::Base
-        include Gitlab::Event::Action::UsersProject
-
         class << self
           def can_build?(action, data)
-            known_action = known_action? action
+            known_action = known_action? action, ::UsersProject.available_actions
             known_source = data.is_a? ::UsersProject
             known_source && known_action
           end
@@ -22,7 +20,7 @@ module Gitlab
             when :deleted
             end
 
-            ::Event.new(action: ::Event::Action.action_by_name(meta[:action]),
+            ::Event.new(action: meta[:action],
                         source: source, data: data.to_json, author: user, target: target)
           end
         end
