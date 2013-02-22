2bd7b52e79906786733adcbcda9bd27b88718898
Andrew8xx8 <avk@8xx8.ru>
2013-02-22 19:33:16 +0400

    Aaction now symbols

--- lib/gitlab/event/builder/protected_branch.rb
+++ lib/gitlab/event/builder/protected_branch.rb
@@ -2,11 +2,9 @@ module Gitlab
   module Event
     module Builder
       class ProtectedBranch < Gitlab::Event::Builder::Base
-        include Gitlab::Event::Action::ProtectedBranch
-
         class << self
           def can_build?(action, data)
-            known_action = known_action? action
+            known_action = known_action? action, ::ProtectedBranch.available_actions
             known_source = data.is_a? ::ProtectedBranch
             known_source && known_action
           end
@@ -17,7 +15,7 @@ module Gitlab
 
             target = source
 
-            ::Event.new(action: ::Event::Action.action_by_name(meta[:action]),
+            ::Event.new(action: meta[:action],
                         source: source, data: data.to_json, author: user, target: target)
           end
         end
--- lib/gitlab/event/builder/snippet.rb
+++ lib/gitlab/event/builder/snippet.rb
@@ -2,11 +2,9 @@ module Gitlab
   module Event
     module Builder
       class Snippet < Gitlab::Event::Builder::Base
-        include Gitlab::Event::Action::Snippet
-
         class << self
           def can_build?(action, data)
-            known_action = known_action? action
+            known_action = known_action? action, ::Snippet.available_actions
             known_source = data.is_a? ::Snippet
             known_source && known_action
           end
@@ -17,7 +15,7 @@ module Gitlab
 
             target = source
 
-            ::Event.new(action: ::Event::Action.action_by_name(meta[:action]),
+            ::Event.new(action: meta[:action],
                         source: source, data: data.to_json, author: user, target: target)
           end
         end
