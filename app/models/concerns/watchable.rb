112b38c582408aef52bf5aa437c12a9faeafc9c8
Andrew8xx8 <avk@8xx8.ru>
2013-02-22 19:31:24 +0400

    Actions moved to models

---
+++ app/models/concerns/watchable.rb
@@ -0,0 +1,23 @@
+module Watchable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def actions_to_watch(actions)
+      return if actions.empty?
+
+      @actions ||= actions.select { |action| Event::Action.action_exists?(action) }
+    end
+
+    def available_actions
+      @actions
+    end
+  end
+
+  def watch_status?(user)
+
+  end
+
+  def can_watch?(user)
+
+  end
+end
