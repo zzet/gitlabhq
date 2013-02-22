c6f9db447bfdf3fed5c4f922d7cb7bab218c6404
Andrew8xx8 <avk@8xx8.ru>
2013-02-22 19:33:16 +0400

    Aaction now symbols

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(:version => 20130220133245) do
 
   create_table "event_subscriptions", :force => true do |t|
     t.integer  "user_id"
-    t.integer  "action"
+    t.string   "action"
     t.integer  "target_id"
     t.string   "target_type"
     t.integer  "source_id"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(:version => 20130220133245) do
 
   create_table "events", :force => true do |t|
     t.integer  "author_id"
-    t.integer  "action"
+    t.string   "action"
     t.integer  "source_id"
     t.string   "source_type"
     t.integer  "target_id"
