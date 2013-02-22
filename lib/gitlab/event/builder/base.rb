ba39d5e06c742fc44e92861b7f38776ec50aab52
Andrew8xx8 <avk@8xx8.ru>
2013-02-22 19:33:16 +0400

    Aaction now symbols

--- lib/gitlab/event/builder/base.rb
+++ lib/gitlab/event/builder/base.rb
@@ -10,6 +10,22 @@ module Gitlab
           def build(action, target, user, data)
             raise NotImplementedError
           end
+
+          def known_action?(action, available_actions)
+            meta = parse_action(action)
+            available_actions.include? meta[:action]
+          end
+
+          private
+
+          def parse_action(action)
+            info = action.split "."
+            info.shift # Shift "gitlab"
+            {
+              action: info.shift.to_sym,
+              details: info
+            }
+          end
         end
       end
     end
