5da0ae1373d91257a164f34eaa34febc7b2c0b55
Andrey Kumanyaev <me@zzet.org>
2013-02-23 01:56:32 +0400

    prepare staging deploy

---
+++ config/deploy/staging.rb
@@ -0,0 +1,12 @@
+set :rails_env, "production"
+set :branch, 'staging'
+set :user, 'gitlab'
+set :keep_releases, 5
+set :deploy_to, '/rest/u/apps/gitlab'
+
+set :db_adapter,     'postgres'
+set :bundle_without, %w[development test] + (%w[mysql postgres] - [db_adapter])
+
+role :web, "10.40.42.123"
+role :app, "10.40.42.123"
+role :db,  "10.40.42.123", :primary => true
