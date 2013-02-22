ee4079b2db0e26827aa6ecbd56f97796727083c7
Andrew8xx8 <avk@8xx8.ru>
2013-02-22 19:33:16 +0400

    Aaction now symbols

--- lib/gitlab/event/subscriptions/users_project.rb
+++ lib/gitlab/event/subscriptions/users_project.rb
@@ -2,8 +2,6 @@ module Gitlab
   module Event
     module Subscriptions
       class UsersProject < Gitlab::Event::Subscriptions::Base
-        include Gitlab::Event::Action::UsersProject
-
         class << self
         end
 
