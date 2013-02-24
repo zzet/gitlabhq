1f5af63eed87fb8d0cc0614ba7e4eb9926bc80d5
Andrey Kumanyaev <me@zzet.org>
2013-02-24 13:39:35 +0400

    Update notifications

---
+++ app/workers/mail_notification_worker.rb
@@ -0,0 +1,9 @@
+class MailNotificationWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :mail_notifications
+
+  def perform(notification)
+    NotificationService.process_notification(notification)
+  end
+end
