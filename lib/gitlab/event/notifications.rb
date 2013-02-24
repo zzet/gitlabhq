8f833d25586887b751d378c0be51944626805fa6
Andrey Kumanyaev <me@zzet.org>
2013-02-24 13:39:35 +0400

    Update notifications

--- lib/gitlab/event/notifications.rb
+++ lib/gitlab/event/notifications.rb
@@ -10,9 +10,25 @@ module Gitlab
             subscription.notifications.create(event: event)
           end
         end
+
+        def process_notification(notificaton)
+          action = Event::Action.action_to_s(notification.event.action)
+          target = notification.event.target.class_name.to_s
+          source = notification.event.source.class_name.to_s
+
+          mail_method = "#{action}_#{target}_#{source}email"
+
+          if EventNotificationMailer.respond_to?(mail_method)
+            EventNotificationMailer.send(mail_method, notification)
+          else
+            Rails.logger.info "undefined #{mail_method}"
+            EventNotificationMailer.dafault_email(notification)
+          end
+        end
+
       end
+
     end
   end
 end
 
-ActiveSupport::Notifications.subscribe(/gitlab/, EventNotificationWorker.new)
