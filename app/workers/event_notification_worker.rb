91ae0f44142e156a35314d0503f0ab63a6862820
Andrey Kumanyaev <me@zzet.org>
2013-02-24 21:33:19 +0400

    Update notifications

--- app/workers/event_notification_worker.rb
+++ app/workers/event_notification_worker.rb
@@ -1,7 +1,13 @@
 class EventNotificationWorker
-  def call(name, started, finished, unique_id, data)
+  cattr_accessor :queue
+
+  def self.call(name, started, finished, unique_id, data)
     Rails.logger.info "Triggered action: " << name
 
     Gitlab::Event::Factory.create_events(name, data)
+  end
+
+  def self.subscribed?
+    queue.present?
   end
 end
