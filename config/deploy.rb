b7eab2a74f2e49880172219110fdec2aef82bdb5
Andrey Kumanyaev <me@zzet.org>
2013-02-25 02:18:11 +0400

    update migrations and deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :application, "gitlab"
 set :rvm_type, :system
 
 set :scm, :git
-set :repository, "git://github.com/Undev/gitlabhq.git"# "git@gitlab.home:gitlabhq.git"
+set :repository, "git://github.com/zzet/gitlabhq.git"# "git@gitlab.home:gitlabhq.git"
 
 set :use_sudo, false
 set :ssh_options, :forward_agent => true
