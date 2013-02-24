1db6f8ed1008d05d2857f2d7e13c64c85970fc6e
Andrey Kumanyaev <me@zzet.org>
2013-02-24 13:38:44 +0400

    Action should triggered by Action, No totification

--- lib/gitlab/event/notifications.rb
+++ lib/gitlab/event/notifications.rb
@@ -1,20 +1,8 @@
 module Gitlab
   module Event
     class Notifications
-      cattr_accessor :current_user
 
       class << self
-        def trigger(action, source, user = nil, data = nil, detailed_event = "")
-          data = source if data.blank?
-          source_name = source.class.name unless source.is_a? String
-          action = action.to_s unless action.is_a? String
-          user = current_user if user.blank?
-
-          event = "gitlab.#{action}.#{source_name}".downcase
-          event << ".#{detailed_event}" unless detailed_event.blank?
-
-          ActiveSupport::Notifications.instrument event, {source: source, user: user, data: data}
-        end
 
         def create_notifications(event)
           subscriptions = ::Event::Subscription.by_target(event.target).by_source_type(event.source)
