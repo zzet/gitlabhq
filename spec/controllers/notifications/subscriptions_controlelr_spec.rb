0a6382a03fe16f8d063762e9c105c7d73ed6df19
Andrew8xx8 <avk@8xx8.ru>
2013-02-25 18:16:48 +0400

    Tests improved

--- spec/controllers/notifications/subscriptions_controlelr_spec.rb
+++ spec/controllers/notifications/subscriptions_controlelr_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Notifications::SubscriptionsController do
-  let(:project) { create(:project) }
-  let(:group)   { create(:group) }
-  let(:user)    { create(:user) }
+  let(:project)   { create(:project) }
+  let(:group)     { create(:group) }
+  let(:user_team) { create(:user_team) }
+  let(:user)      { create(:user) }
 
   before do
     sign_in(user)
@@ -22,6 +23,36 @@ describe Notifications::SubscriptionsController do
       post :create, :entity => { id: group.id,  type: :group}, format: :html
 
       group.watched_by?(user).should be_true 
+    end
+
+    it "should register subscription on team" do
+      post :create, :entity => { id: user_team.id,  type: :user_team}, format: :html
+
+      user_team.watched_by?(user).should be_true 
+    end
+
+    it "should unregister subscription on project" do
+      SubscriptionService.subscribe(user, :all, project, :all)
+
+      delete :destroy, :entity => { id: project.id,  type: :project}, format: :html
+
+      project.watched_by?(user).should be_false 
+    end
+
+    it "should unregister subscription on group" do
+      SubscriptionService.subscribe(user, :all, group, :all)
+
+      delete :destroy, :entity => { id: group.id,  type: :group}, format: :html
+
+      group.watched_by?(user).should be_false 
+    end
+
+    it "should unregister subscription on team" do
+      SubscriptionService.subscribe(user, :all, user_team, :all)
+
+      delete :destroy :entity => { id: user_team.id,  type: :user_team}, format: :html
+
+      user_team.watched_by?(user).should be_false 
     end
   end
 end
