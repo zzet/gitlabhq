7b7413f1522a4dc31fc2131aebbe6b831c4e88ae
Andrey Kumanyaev <me@zzet.org>
2013-03-01 23:19:30 +0400

    NO destroy Events with destroy entity

--- app/models/protected_branch.rb
+++ app/models/protected_branch.rb
@@ -19,7 +19,7 @@ class ProtectedBranch < ActiveRecord::Base
   validates :name, presence: true
   validates :project, presence: true
 
-  has_many :events,         as: :source,    dependent: :destroy
+  has_many :events,         as: :source
   has_many :subscriptions,  conditions: { action: "some_action" }
   has_many :notifications,  through: :subscriptions
   has_many :subscribers,    through: :subscriptions
