e0f3780961b9aaa4a0b47aa23bbb536d64c103a8
Andrey Kumanyaev <me@zzet.org>
2013-03-01 04:56:00 +0400

    update mails && logic

--- app/workers/event_subscription_worker.rb
+++ app/workers/event_subscription_worker.rb
@@ -1,7 +1,15 @@
 class EventSubscriptionWorker
   def self.call(name, started, finished, unique_id, data)
-    Rails.logger.info "Create subscription by action: " << name
 
-    Gitlab::Event::Subscription.create_subscriprions_by_target(data[:source])
+    gitlab, action, source = name.split "."
+
+    begin
+      if (Event::Subscription.global_entity_to_subscription.include? source.to_sym) && (action.to_sym == :created)
+        Rails.logger.info "Create subscription by action: " << name
+        Gitlab::Event::Subscription.create_subscriprions_by_target(data[:source])
+      end
+    rescue
+      Rails.logger.warn "Error while process create subscription on action #{name}"
+    end
   end
 end
