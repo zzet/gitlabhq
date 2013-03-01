677dbc392270830d07963ae3279f80661a1d9525
Andrey Kumanyaev <me@zzet.org>
2013-03-01 21:58:32 +0400

    Disable old notifications

--- app/observers/merge_request_observer.rb
+++ app/observers/merge_request_observer.rb
@@ -1,10 +1,6 @@
 class MergeRequestObserver < BaseObserver
   cattr_accessor :current_user
 
-  def after_create(merge_request)
-    notification.new_merge_request(merge_request, current_user)
-  end
-
   def after_close(merge_request, transition)
     Note.create_status_change_note(merge_request, current_user, merge_request.state)
 
@@ -17,9 +13,5 @@ class MergeRequestObserver < BaseObserver
 
   def after_reopen(merge_request, transition)
     Note.create_status_change_note(merge_request, current_user, merge_request.state)
-  end
-
-  def after_update(merge_request)
-    notification.reassigned_merge_request(merge_request) if merge_request.is_being_reassigned?
   end
 end
