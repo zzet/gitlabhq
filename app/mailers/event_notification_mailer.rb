ff1deae24a29f8eb7a35e96d1338a3f603a43e65
Andrey Kumanyaev <me@zzet.org>
2013-03-04 15:46:31 +0400

    Fix joined_user_users_project_email

--- app/mailers/event_notification_mailer.rb
+++ app/mailers/event_notification_mailer.rb
@@ -623,10 +623,11 @@ class EventNotificationMailer < ActionMailer::Base
     @notification = notification
     @event = @notification.event
     @user = @event.author
-    @source = @event.source
-    @target = @event.target
+    @up = @source = @event.source
+    @project = @up.project
+    @member = @target = @event.target
 
-    mail(bcc: @notification.subscriber.email, subject: "[Gitlab] User #{@target.user.name} was added to #{@source.name} project by #{@user.name} [joined]")
+    mail(bcc: @notification.subscriber.email, subject: "[Gitlab] User #{@member.name} was added to #{@project.name_with_namespace} project by #{@user.name} [joined]")
   end
 
   def joined_user_user_team_user_relationship_email(notification)
