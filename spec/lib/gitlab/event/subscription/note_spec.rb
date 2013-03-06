2b9aadb9e06ceafa82e421944d804f240fe13334
Andrey Kumanyaev <me@zzet.org>
2013-03-07 03:44:31 +0400

    fix after renaming

--- spec/lib/gitlab/event/subscription/note_spec.rb
+++ spec/lib/gitlab/event/subscription/note_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
-describe Gitlab::Event::Subscriptions::Group do
+describe Gitlab::Event::Subscription::Note do
   it "should respond to :subscribe method" do
-    Gitlab::Event::Subscriptions::Group.should respond_to :can_subscribe?
+    Gitlab::Event::Subscription::Note.should respond_to :can_subscribe?
   end
 
-  describe "Group subscribe" do
+  describe "Note subscribe" do
     before do
       @user = create :user
     end
 
-    it "should subscribe user on exist group changes" do
-      source = create :group
+    it "should subscribe user on exist note changes" do
+      source = create :note
       target = source
       action = :updated
 
@@ -22,9 +22,9 @@ describe Gitlab::Event::Subscriptions::Group do
       subscription.should be_persisted
     end
 
-    it "should subscribe user on all groups changes by subscribe with symbol" do
-      source = :group
-      target = create :group
+    it "should subscribe user on all notes changes by subscribe with symbol" do
+      source = :note
+      target = create :issue
       action = :created
 
       Gitlab::Event::Subscription.subscribe(@user, action, target, source)
@@ -34,9 +34,9 @@ describe Gitlab::Event::Subscriptions::Group do
       subscription.should be_persisted
     end
 
-    it "should subscribe user on all groups changes by subscribe with Class name" do
-      source = Group
-      target = create :group
+    it "should subscribe user on all notes changes by subscribe with Class name" do
+      source = Note
+      target = create :issue
       action = :created
 
       Gitlab::Event::Subscription.subscribe(@user, action, target, source)
@@ -46,9 +46,9 @@ describe Gitlab::Event::Subscriptions::Group do
       subscription.should be_persisted
     end
 
-    it "should subscribe user on exist group :project adds" do
-      target = create :group
-      source = :project
+    it "should subscribe user on exist note :note adds" do
+      target = create :note
+      source = :note
       action = :created
 
       Gitlab::Event::Subscription.subscribe(@user, action, target, source)
--- spec/lib/gitlab/event/subscriptions/base_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Gitlab::Event::Subscriptions::Base do
-  it "should respond to :subscribe method" do
-    Gitlab::Event::Subscriptions::Base.should respond_to :can_subscribe?
-  end
-end
