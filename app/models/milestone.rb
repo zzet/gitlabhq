a7ad6475f450eb7d44fb542e06f614e44d8b56de
Andrey Kumanyaev <me@zzet.org>
2013-03-07 15:29:50 +0400

    update relations with subscriptons. Destroy subscription if target destroyed.

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -24,7 +24,7 @@ class Milestone < ActiveRecord::Base
   has_many :merge_requests
 
   has_many :events,         as: :source
-  has_many :subscriptions,  conditions: { target_id: self, target_type: self }
+  has_many :subscriptions,  as: :target, depended: :destroy, class_name: Event::Subscription
   has_many :notifications,  through: :subscriptions
   has_many :subscribers,    through: :subscriptions
 
