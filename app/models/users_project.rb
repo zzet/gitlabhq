ff5b0ae3b7aa6fbceac1925d0e2ef0dd60646cf7
Andrey Kumanyaev <me@zzet.org>
2013-03-07 15:29:50 +0400

    update relations with subscriptons. Destroy subscription if target destroyed.

--- app/models/users_project.rb
+++ app/models/users_project.rb
@@ -25,7 +25,7 @@ class UsersProject < ActiveRecord::Base
   belongs_to :project
 
   has_many :events,         as: :source
-  has_many :subscriptions,  conditions: { action: "some_action" }
+  has_many :subscriptions,  as: :target, depended: :destroy, class_name: Event::Subscription
   has_many :notifications,  through: :subscriptions
   has_many :subscribers,    through: :subscriptions
 
