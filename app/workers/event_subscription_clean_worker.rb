6496efd133d21ca5dc64f49ad42e7b825827d881
Andrey Kumanyaev <me@zzet.org>
2013-03-13 02:07:21 +0400

    Fix simple bugs in subscription logic

--- app/workers/event_subscription_clean_worker.rb
+++ app/workers/event_subscription_clean_worker.rb
@@ -5,12 +5,12 @@ class EventSubscriptionCleanWorker
 
     begin
       # TODO. Check, if need to destroy subscriptions while user transfer prject between groups, for example
-      if (Event::Subscription.global_entity_to_subscription.include? source.to_sym) && (action.to_sym == :deleted)
+      if (Event::Subscription.global_entity_to_subscription.include? source.to_sym) && ([:deleted].include? action.to_sym)
         Rails.logger.info "Delete subscription by action: " << name
         Gitlab::Event::Subscription.destroy_subscriprions_by_target(data[:source])
       end
     rescue
-      Rails.logger.warn "Error while process create subscription on action #{name}"
+      Rails.logger.warn "Error while process destroy subscription on action #{name}"
     end
   end
 end
