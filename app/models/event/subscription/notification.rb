357c7fcc0fec5c10f07baa2b158750e51a87c30e
Andrey Kumanyaev <me@zzet.org>
2013-03-13 02:08:53 +0400

    Store subscriber id in notifications to save process if user unsubscribe early notification processing

--- app/models/event/subscription/notification.rb
+++ app/models/event/subscription/notification.rb
@@ -1,18 +1,16 @@
 class Event::Subscription::Notification < ActiveRecord::Base
-  attr_accessible :event_id, :event, :notification_state, :notified_at, :subscription_id
+  attr_accessible :event_id, :event, :notification_state, :notified_at, :subscription_id, :subscriber, :subscriber_id
 
   belongs_to :event
+  belongs_to :subscriber,   class_name: User
   belongs_to :subscription, class_name: Event::Subscription
 
+  validates :subscriber,   presence: true
   validates :event,        presence: true
   validates :subscription, presence: true
 
   scope :pending, -> { where(notification_state: :new) }
   scope :instantaneous, -> { pending.where(notification_interval: 0) }
-
-  def subscriber
-    subscription.user
-  end
 
   state_machine :notification_state, initial: :new do
     state :new
