63b8483bb0f71d0e057570ef53588f6fd03dfa44
Andrey Kumanyaev <me@zzet.org>
2013-03-13 02:08:53 +0400

    Store subscriber id in notifications to save process if user unsubscribe early notification processing

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130410175022) do
     t.datetime "notified_at"
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
+    t.integer  "subscriber_id"
   end
 
   create_table "event_subscriptions", :force => true do |t|
