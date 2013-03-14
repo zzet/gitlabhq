f0c098ec1051e962c0471b6225ffb42b210819cf
Andrey Kumanyaev <me@zzet.org>
2013-03-14 17:12:26 +0400

    Show activity also in public projects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by_username!(params[:username])
-    @projects = @user.authorized_projects.where('projects.id in (?)', current_user.authorized_projects.map(&:id))
+    @projects = @user.authorized_projects.merge(Project.public_only)
     @events = @user.recent_events.where(project_id: @projects.map(&:id)).limit(20)
   end
 end
