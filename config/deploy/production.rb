f37151068f8ab8760fc652f14e6c8890fe0c1add
Andrey Kumanyaev <me@zzet.org>
2013-03-26 17:01:24 +0400

    Use rvm on production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@ set :branch, 'new_staging'
 set :user, 'gitlab'
 set :keep_releases, 5
 set :deploy_to, '/rest/u/apps/gitlab'
+set :rvm_ruby_string, 'ruby-1.9.3-p194@gitlab'
 
 set :db_adapter,     'postgres'
 set :bundle_without, %w[development test] + (%w[mysql postgres] - [db_adapter])
