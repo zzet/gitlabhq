ded006a8abc17e5d067ad363a2ba098620f0890c
Andrey Kumanyaev <me@zzet.org>
2013-03-26 15:10:34 +0400

    Add production configs
    Conflicts:
    
    	config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,13 @@
-set :user, 'gitlab_production'
 set :rails_env, 'production'
+set :branch, 'new_legacy_staging'
+#set :branch, 'new_staging'
+set :user, 'gitlab'
+set :keep_releases, 5
+set :deploy_to, '/rest/u/apps/gitlab'
 
-role :db,  "10.40.37.207", primary: true
-role :app, "10.40.37.207"
-role :web, "10.40.37.207"
+set :db_adapter,     'postgres'
+set :bundle_without, %w[development test]
 
-set :branch, "master"
-set :deploy_to, "/u/apps/gitlab"
+role :web, "192.168.249.67"
+role :app, "192.168.249.67"
+role :db,  "192.168.249.67", primary: true
