83e3f81cad8f7239df2fcf06ea0da313d77ff2c0
Andrey Kumanyaev <me@zzet.org>
2013-04-01 21:55:19 +0400

    Fix abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -92,16 +92,16 @@ class Ability
         :admin_merge_request,
         :admin_note,
         :admin_wiki,
+        :change_namespace,
+        :change_public_mode,
+        :rename_project,
+        :remove_project,
         :admin_project
       ]
     end
 
     def project_admin_rules
       project_master_rules + [
-        :change_namespace,
-        :change_public_mode,
-        :rename_project,
-        :remove_project
       ]
     end
 
