f9ef3d2e0dae30f5afdd3549bda4edb6f992569b
Andrey Kumanyaev <me@zzet.org>
2013-04-03 14:32:33 +0400

    Fix observer

--- app/observers/project_activity_cache_observer.rb
+++ app/observers/project_activity_cache_observer.rb
@@ -2,7 +2,7 @@ class ProjectActivityCacheObserver < ActiveRecord::Observer
   observe :event
 
   def after_create(event)
-    project.update_attribute(:last_activity_at, event.created_at)
+    event.project.update_attribute(:last_activity_at, event.created_at) if event.project
   end
 end
 
