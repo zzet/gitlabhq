26e685627305299080dcc6804ffce35d76eee287
Andrey Kumanyaev <me@zzet.org>
2013-05-17 13:25:30 +0400

    Fix access to files via file tokens refs fixes #27038

--- app/controllers/blob_controller.rb
+++ app/controllers/blob_controller.rb
@@ -2,34 +2,12 @@
 class BlobController < ProjectResourceController
   include ExtractsPath
 
-  skip_before_filter :authenticate_user!, :reject_blocked, :set_current_user_for_observers, :add_abilities, if: :file_auth_token_present?
-
   # Authorize
-  before_filter :before_filters_for_users
+  before_filter :authorize_read_project!
+  before_filter :authorize_code_access!
+  before_filter :require_non_empty_project
 
   def show
     @blob = Gitlab::Git::Blob.new(@repository, @commit.id, @ref, @path)
-  end
-
-  protected
-
-  def file_auth_token_present?
-    params[:file_auth_token].present?
-  end
-
-  def before_filters_for_users
-    if file_auth_token_present?
-      token = params[:file_auth_token]
-      available_token = FileToken.for_project(@project).find_by_file(@path)
-
-      if available_token.blank? || available_token.token != token
-        not_found!
-      end
-    else
-      authorize_read_project!
-      authorize_code_access!
-    end
-
-    require_non_empty_project
   end
 end
--- app/controllers/raw_controller.rb
+++ app/controllers/raw_controller.rb
@@ -7,6 +7,11 @@ class RawController < ProjectResourceController
   before_filter :authorize_code_access!
   before_filter :require_non_empty_project
 
+  skip_before_filter :authenticate_user!, :reject_blocked, :set_current_user_for_observers, :add_abilities, if: :file_auth_token_present?
+
+  # Authorize
+  before_filter :before_filters_for_users
+
   def show
     @blob = Gitlab::Git::Blob.new(@repository, @commit.id, @ref, @path)
 
@@ -21,5 +26,27 @@ class RawController < ProjectResourceController
       not_found!
     end
   end
-end
 
+  protected
+
+  def file_auth_token_present?
+    params[:file_auth_token].present?
+  end
+
+  def before_filters_for_users
+    if file_auth_token_present?
+      token = params[:file_auth_token]
+      available_token = FileToken.for_project(@project).find_by_file(@path)
+
+      if available_token.blank? || available_token.token != token
+        not_found!
+      end
+    else
+      authorize_read_project!
+      authorize_code_access!
+    end
+
+    require_non_empty_project
+  end
+
+end
