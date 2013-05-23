052ad5fd16dd452556411cede663e089e67201b0
Andrew8xx8 <avk@8xx8.ru>
2013-05-23 16:40:50 +0400

    Namespace fixed

--- app/mailers/event_notification_mail_preview.rb
+++ app/mailers/event_notification_mail_preview.rb
@@ -2,7 +2,7 @@ class EventNotificationMailPreview < MailView
   extend Gitlab::Event::MailViewDsl
 
   preview :pushed_project_push_summary_email do
-    event = Event.find_by_action :pushed
+    event = ::Event.find_by_action :pushed
     Event::Subscription::Notification.find_by_event_id event.id
   end
 end
