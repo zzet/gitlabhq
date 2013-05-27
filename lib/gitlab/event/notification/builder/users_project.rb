17d8242b2bd7994a931f341f5f67d23b380f33cb
Andrew8xx8 <avk@8xx8.ru>
2013-05-27 18:25:39 +0400

    user_echo refactored

--- lib/gitlab/event/notification/builder/users_project.rb
+++ lib/gitlab/event/notification/builder/users_project.rb
@@ -1,6 +1,16 @@
 class Gitlab::Event::Notification::Builder::UsersProject < Gitlab::Event::Notification::Builder::Base
 
   class << self
+    def can_build?(subscription, event_data)
+      return false if team_already_have?(event)
+
+      super
+    end
+
+    def team_already_have?(event)
+      team_events = ::Event.where(target_id: event.target.id, target_type: event.target.class, created_at: (Time.now - 5.minutes)..Time.now)
+      team_events.any?
+    end
   end
 
 end
