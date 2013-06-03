a270f93b3e687d55d01d01b5b76ce411f26e8c46
Andrey Kumanyaev <me@zzet.org>
2013-06-03 22:11:55 +0400

    fix notification observer

--- app/observers/notification_observer.rb
+++ app/observers/notification_observer.rb
@@ -2,7 +2,7 @@ class NotificationObserver < ActiveRecord::Observer
   observe Event::Subscription::Notification
 
   def after_create(notification)
-    Sidekiq::Client.enqueue_to(:mail_notifications, MailNotificationWorker, notification)
+    Sidekiq::Client.enqueue_to(:mail_notifications, MailNotificationWorker, notification.id)
   end
 
 end
