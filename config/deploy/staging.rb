04bc46f5cf06f02cbb76c5d54fdc0533b58feef4
Andrey Kumanyaev <me@zzet.org>
2013-06-07 19:36:38 +0400

    deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@ set :keep_releases, 5
 set :deploy_to, '/rest/u/apps/gitlab'
 
 set :db_adapter,     'postgres'
-set :bundle_without, %w[development test] + (%w[mysql postgres] - [db_adapter])
+set :bundle_without, %w[development] + (%w[mysql postgres] - [db_adapter])
 
 role :web, "10.40.42.123"
 role :app, "10.40.42.123"
