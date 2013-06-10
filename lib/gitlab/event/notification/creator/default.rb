e38fbae9fe324a089850087d188bd625d2ce1153
Andrey Kumanyaev <me@zzet.org>
2013-06-10 15:11:27 +0400

    remake ajacent notifications

--- lib/gitlab/event/notification/creator/default.rb
+++ lib/gitlab/event/notification/creator/default.rb
@@ -12,6 +12,8 @@ class Gitlab::Event::Notification::Creator::Default
       end
     end
 
+    create_adjacent_notifications(event)
+
     notifications
   end
 
@@ -23,6 +25,53 @@ class Gitlab::Event::Notification::Creator::Default
   end
 
   private
+
+  def create_adjacent_notifications(event)
+    subscription_target = nil
+    subscription_source = nil
+
+    case event.target
+    when Project
+      project = event.target
+      project = event.source
+      namespace = project.namespace
+
+      if namespace
+        subscription_target = namespace.type == "Group" ? namespace.becomes(Group) : namespace.becomes(User)
+        subscription_source = :project
+      end
+    end
+
+    if subscription_target && subscription_source
+      subscribe_users_to_adjacent_resources(subscription_target, subscription_source)
+
+      subscriptions = ::Event::Subscription.by_target(subscription_target).by_source_type_hard(subscription_source)
+
+      subscriptions.each do |subscription|
+        if build_notification?(subscription, event)
+          air_subscriptions = ::Event::Subscription.by_user(subscription.user).by_target(event.target).by_source_type_hard(event.source)
+          if air_subscriptions.blank?
+            subscription.notifications.create(event: event, subscriber: subscription.user)
+          end
+        end
+      end
+
+    end
+  end
+
+  def subscribe_users_to_adjacent_resources(target, source)
+    ss = Event::Subscription::NotificationSetting.where(adjacent_changes: true)
+    ss.each do |settings|
+      user = settings.user
+      subscriptions = Event::Subscription.by_user(user).by_target(target).by_source_type(:all)
+      if subscriptions.any?
+        tageted_subscriptions = Event::Subscription.by_user(user).by_target(target).by_source_type_hard(source)
+        SubscriptionService.subscribe(user, :all, target, source) if tageted_subscriptions.blank?
+      end
+    end
+  end
+
+
 
   def parent_event_for(event)
     event.parent_event
