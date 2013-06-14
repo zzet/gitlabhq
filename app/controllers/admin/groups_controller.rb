a8a5b16d0ea2ec8f8d6696d23155582ccf53b1fc
Andrey Kumanyaev <me@zzet.org>
2013-06-14 11:04:31 +0400

    save

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -81,9 +81,7 @@ class Admin::GroupsController < Admin::ApplicationController
   end
 
   def destroy
-    @group.truncate_teams
-
-    @group.destroy
+    ::Groups::RemoveContext.new(group, current_user).execute
 
     redirect_to admin_groups_path, notice: 'Group was successfully deleted.'
   end
