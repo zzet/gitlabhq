0fb58deed80bd57f74b95f87672465277d907eb6
Andrey Kumanyaev <me@zzet.org>
2013-07-02 14:31:32 +0400

    remove key converting

--- app/contexts/projects/teams/create_relation_context.rb
+++ app/contexts/projects/teams/create_relation_context.rb
@@ -2,7 +2,6 @@ module Projects
   module Teams
     class CreateRelationContext < Projects::BaseContext
       def execute
-        params.symbolize_keys!
         unless params[:team_id].blank?
           team = UserTeam.find(params[:team_id])
           params[:project_ids] = [project.id]
