31a86b75e9ae861eb2ad3430b37ab9d5dfcd0bec
Andrey Kumanyaev <me@zzet.org>
2013-07-09 21:14:53 +0400

    No notification if commit author subscribed on project changes

--- lib/gitlab/event/notification/creator/note.rb
+++ lib/gitlab/event/notification/creator/note.rb
@@ -6,10 +6,19 @@ class Gitlab::Event::Notification::Creator::Note < Gitlab::Event::Notification::
   end
 
   def can_create_for_commit_author?(event)
-    event.source.commit_author.present? && event.source.commit_author != event.author
+    correct_commit_author?(event) && no_notification?(event, event.source.commit_author)
   end
 
   def create_notification_for_commit_author(event)
     ::Event::Subscription::Notification.create(event: event, subscriber: event.source.commit_author)
+  end
+
+  def correct_commit_author?(event)
+    event.source.commit_author.present? && event.source.commit_author != event.author
+  end
+
+  def no_notification?(event, user)
+    return false if user.blank?
+    ::Event::Subscription::Notification.where(event_id: event, subscriber_id: user).any?
   end
 end
