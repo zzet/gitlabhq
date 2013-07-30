3022754e2139d6ceb47ce78bf3e87d154e80c37c
Andrey Kumanyaev <me@zzet.org>
2013-07-30 18:13:33 +0400

    fix display last push on team page && fix bug

--- app/observers/project_activity_cache_observer.rb
+++ app/observers/project_activity_cache_observer.rb
@@ -4,7 +4,7 @@ class ProjectActivityCacheObserver < BaseObserver
   def after_create(event)
     if event.project
       event.project.update_column(:last_activity_at, event.created_at)
-      event.project.update_column(:last_pushed_at, event.created_at) if event.action = 5
+      event.project.update_column(:last_pushed_at, event.created_at) if event.action == 5
     end
   end
 end
