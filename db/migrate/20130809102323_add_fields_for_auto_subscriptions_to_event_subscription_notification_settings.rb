f942f57163d76b4ba7126e7cc31259fbd201264d
Andrey Kumanyaev <me@zzet.org>
2013-08-12 11:48:28 +0400

    Auto subscribe to own projects buttons enabled by default

--- db/migrate/20130809102323_add_fields_for_auto_subscriptions_to_event_subscription_notification_settings.rb
+++ db/migrate/20130809102323_add_fields_for_auto_subscriptions_to_event_subscription_notification_settings.rb
@@ -1,6 +1,6 @@
 class AddFieldsForAutoSubscriptionsToEventSubscriptionNotificationSettings < ActiveRecord::Migration
   def change
-    add_column :event_subscription_notification_settings, :subscribe_if_owner, :boolean
-    add_column :event_subscription_notification_settings, :subscribe_if_developer, :boolean
+    add_column :event_subscription_notification_settings, :subscribe_if_owner, :boolean, default: true
+    add_column :event_subscription_notification_settings, :subscribe_if_developer, :boolean, default: true
   end
 end
