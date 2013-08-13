bb9bc4a3e6132edfe9e601ed83341fbd4913da7f
Andrey Kumanyaev <me@zzet.org>
2013-08-13 14:51:46 +0400

    On user page show correct projects list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_username!(params[:username])
-    @projects = @current_user.known_projects.where(id: @user.known_projects)
-    @events = @user.recent_events.where(project_id: @projects).limit(params[:limit]).offset(params[:offset])
+    @projects = @current_user.known_projects.where(id: @user.authorized_projects)
+    @event_projects = @current_user.known_projects
+    @events = @user.recent_events.where(project_id: @event_projects).limit(params[:limit]).offset(params[:offset])
 
     @title = @user.name
   end
