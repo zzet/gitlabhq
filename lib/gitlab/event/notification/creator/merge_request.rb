4cf6f41948a836fa504994b569e8074d21656d44
Andrey Kumanyaev <me@zzet.org>
2013-08-29 21:23:51 +0400

    fix deploy bug

--- lib/gitlab/event/notification/creator/merge_request.rb
+++ lib/gitlab/event/notification/creator/merge_request.rb
@@ -6,7 +6,7 @@ class Gitlab::Event::Notification::Creator::MergeRequest < Gitlab::Event::Notifi
 
     notifications << create_notification_for_assigned(event) if can_create_for_assigned?(event)
 
-    notifications << create_notification_for_mentioned_users(event, notifications)
+    notifications << create_notification_for_mentioned_users(event, notifications.flatten)
   end
 
   def can_create_for_assigned?(event)
@@ -39,7 +39,7 @@ class Gitlab::Event::Notification::Creator::MergeRequest < Gitlab::Event::Notifi
   end
 
   def create_notification_for_mentioned_users(event, notifications)
-    notified_user_ids = notifications.map { |n| n.subscriber_id }
+    notified_user_ids = notifications.map { |n| n.user_id }
     user_to_notify = event.source.mentioned_users.reject { |u| notified_user_ids.include?(u.id) }
     user_to_notify.each do |user|
       ::Event::Subscription::Notification.create(event: event, subscriber: user)
