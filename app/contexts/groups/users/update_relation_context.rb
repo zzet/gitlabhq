c92dd96093cd8329ab007dd5537c275990efc98a
Andrey Kumanyaev <me@zzet.org>
2013-09-25 19:54:36 +0400

    Update all actual params in group for user

--- app/contexts/groups/users/update_relation_context.rb
+++ app/contexts/groups/users/update_relation_context.rb
@@ -2,7 +2,7 @@ module Groups
   module Users
     class UpdateRelationContext < Groups::Users::BaseContext
       def execute
-        group_member_relation.update_attributes(params[:users_group])
+        group_member_relation.update_attributes(params)
 
         if group_member_relation.valid?
           receive_delayed_notifications
