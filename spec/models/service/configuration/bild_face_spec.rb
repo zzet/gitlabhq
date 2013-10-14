4beff6a438deedf932e85a83f7007446d39b5638
Andrey Kumanyaev <me@zzet.org>
2013-10-14 10:43:03 +0400

    Repair tests after remove unactual models

--- spec/models/service/configuration/bild_face_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Service::Configuration::BildFace do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/models/service/configuration/build_face_spec.rb
+++ spec/models/service/configuration/build_face_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe ServiceProjectRelationship do
+describe Service::Configuration::BuildFace do
   pending "add some examples to (or delete) #{__FILE__}"
 end
