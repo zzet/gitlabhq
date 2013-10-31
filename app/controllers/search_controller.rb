6084ac8337eeb3f806025c730ae5bf135b2fdd34
Andrey Kumanyaev <me@zzet.org>
2013-10-31 12:24:49 +0400

    Fix selectboxes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,11 @@ class SearchController < ApplicationController
   def show
     result = SearchContext.new(@current_user, params).execute
 
-    @project        = result[:project] if result[:blobs].any?
+    @project        = result[:project]    if result[:blobs].any?
+    @project        = params[:project_id] if params[:project_id].present?
+
+    @group          = params[:group_id]   if params[:group_id].present?
+
     @projects       = result[:projects]
     @merge_requests = result[:merge_requests]
     @issues         = result[:issues]
