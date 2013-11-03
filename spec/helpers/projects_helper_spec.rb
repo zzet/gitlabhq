d89b3ccb6ca0bb5242f4d33856f5512109dfc476
Andrey Kumanyaev <me@zzet.org>
2013-11-03 10:53:22 +0400

    Fix Redmine test

--- spec/helpers/projects_helper_spec.rb
+++ spec/helpers/projects_helper_spec.rb
@@ -4,7 +4,7 @@ describe ProjectsHelper do
   describe '#project_issues_trackers' do
     it "returns the correct issues trackers available" do
       project_issues_trackers.should ==
-          "<option value=\"redmine\">redmine</option>\n" \
+          "<option value=\"redmine\">Redmine</option>\n" \
           "<option value=\"gitlab\">GitLab</option>"
     end
   end
