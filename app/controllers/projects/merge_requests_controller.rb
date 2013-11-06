945cb46e69ab589a621ace091e02d47941cf1388
Andrey Kumanyaev <me@zzet.org>
2013-11-07 02:19:45 +0400

    Improve build status display for commits on merge_request page

--- app/controllers/projects/merge_requests_controller.rb
+++ app/controllers/projects/merge_requests_controller.rb
@@ -44,6 +44,8 @@ class Projects::MergeRequestsController < Projects::ApplicationController
                         noteable_id: @merge_request.id}
     @line_notes = @merge_request.notes.where("line_code is not null")
 
+    @ci_builds = @merge_request.ci_builds
+
     diff_line_count = Commit::diff_line_count(@merge_request.diffs)
     @suppress_diff = Commit::diff_suppress?(@merge_request.diffs, diff_line_count) && !params[:force_show_diff]
     @force_suppress_diff = Commit::diff_force_suppress?(@merge_request.diffs, diff_line_count)
