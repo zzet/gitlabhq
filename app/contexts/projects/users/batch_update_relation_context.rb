d7e99993675a6ad854e07f4fe5a6ed66bb96e553
Andrey Kumanyaev <me@zzet.org>
2014-01-09 17:17:14 +0400

    Fix batch update

--- app/contexts/projects/users/batch_update_relation_context.rb
+++ app/contexts/projects/users/batch_update_relation_context.rb
@@ -3,7 +3,7 @@ module Projects
     class BatchUpdateRelationContext < Projects::BaseContext
       def execute
         user_project_ids = params[:ids].respond_to?(:each) ? params[:ids] : params[:ids].split(',')
-        UsersProject.update(user_project_ids, params[:team_member])
+        UsersProject.where(id: user_project_ids).update_all(project_access: params[:team_member][:project_access])
 
         receive_delayed_notifications
       end
