e2c77570e57b6e1aa8b43c279a07da3f07a90a5c
Andrey Kumanyaev <me@zzet.org>
2014-01-30 17:00:05 +0400

    Add Errbit service

---
+++ app/models/service/errbit.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: services
+#
+#  id                 :integer          not null, primary key
+#  type               :string(255)
+#  title              :string(255)
+#  project_id         :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  state              :string(255)
+#  service_pattern_id :integer
+#  public_state       :string(255)
+#  active_state       :string(255)
+#  description        :text
+#
+
+class Service::Errbit < Service
+  default_title       'Errbit'
+  default_description 'Errbit'
+  service_name        'errbit'
+end
