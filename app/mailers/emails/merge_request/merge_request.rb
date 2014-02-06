9f55893c40b0f497f10ebbc611f70086b45b3021
Andrey Kumanyaev <me@zzet.org>
2014-02-06 21:04:10 +0400

    Fix merge request close email for send without subscriptions

--- app/mailers/emails/merge_request/merge_request.rb
+++ app/mailers/emails/merge_request/merge_request.rb
@@ -45,11 +45,16 @@ class Emails::MergeRequest::MergeRequest < Emails::Base
   end
 
   def closed_email(notification)
-    case notification.subscription.target
-    when Project
+    case notification.subscription
+    when NilClass
       Emails::Project::MergeRequest.closed_email(notification).deliver!
-    when MergeRequest
-      # Send notification
+    else
+      case notification.subscription.target
+      when Project
+        Emails::Project::MergeRequest.closed_email(notification).deliver!
+      when MergeRequest
+        # Send notification
+      end
     end
   end
 end
