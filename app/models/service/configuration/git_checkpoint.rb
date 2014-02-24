2f4e56d5b97e357a5f26a8466db953bd8334e8d1
Andrey Kumanyaev <me@zzet.org>
2014-02-24 20:34:08 +0400

    Add Git Checkpoint service step 1 refs #42051

---
+++ app/models/service/configuration/git_checkpoint.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: service_configuration_git_checkpoints
+#
+#  id               :integer          not null, primary key
+#  service_id       :integer
+#  service_type     :string(255)
+#  token            :string(255)
+#  domain           :string(255)
+#  system_hook_path :string(255)
+#  web_hook_path    :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+class Service::Configuration::GitCheckpoint < ActiveRecord::Base
+  attr_accessible :domain, :service_id, :service_type, :system_hook_path, :web_hook_path, :token
+
+  belongs_to :service, polymorphic: true
+
+  def fields
+    [
+      { type: 'text', name: 'domain',           placeholder: 'http://build-face.undev.cc' },
+      { type: 'text', name: 'system_hook_path', placeholder: '/hooks/gitlab' },
+      { type: 'text', name: 'web_hook_path',    placeholder: '/hooks' },
+    ]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140116231608) do
+ActiveRecord::Schema.define(version: 20140224121926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -347,6 +347,17 @@ ActiveRecord::Schema.define(version: 20140116231608) do
     t.integer  "service_id"
     t.string   "service_type"
     t.string   "token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "service_configuration_git_checkpoints", force: true do |t|
+    t.integer  "service_id"
+    t.string   "service_type"
+    t.string   "token"
+    t.string   "domain"
+    t.string   "system_hook_path"
+    t.string   "web_hook_path"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
