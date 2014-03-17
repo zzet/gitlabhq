b3b83cfd7de5c9e1065962816dd466ab325fa62c
Andrey Kumanyaev <me@zzet.org>
2014-03-17 17:36:33 +0400

    fix 500 while block user

--- app/services/users_service.rb
+++ app/services/users_service.rb
@@ -39,11 +39,17 @@ class UsersService < BaseService
     end
 
     projects_ids.each do |project_id|
-      Elastic::BaseIndexer.perform_async(:update, Project.name, project_id)
+      begin
+        Elastic::BaseIndexer.perform_async(:update, Project.name, project_id)
+      rescue
+      end
     end
 
     teams_ids.each do |team_id|
-      Elastic::BaseIndexer.perform_async(:update, Team.name, team_id)
+      begin
+        Elastic::BaseIndexer.perform_async(:update, Team.name, team_id)
+      rescue
+      end
     end
 
     receive_delayed_notifications
