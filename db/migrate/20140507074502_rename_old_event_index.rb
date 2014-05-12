15be183f6f89da917723af00f4bf8868ad6e5746
skv-headless <skv-headless@yandex.ru>
2014-05-12 15:46:28 +0400

    check renaming index  existing

--- db/migrate/20140507074502_rename_old_event_index.rb
+++ db/migrate/20140507074502_rename_old_event_index.rb
@@ -1,10 +1,29 @@
 class RenameOldEventIndex < ActiveRecord::Migration
   def change
-    rename_index :old_events, 'index_events_on_action', 'index_old_events_on_action'
-    rename_index :old_events, 'index_events_on_author_id', 'index_old_events_on_author_id'
-    rename_index :old_events, 'index_events_on_created_at', 'index_old_events_on_created_at'
-    rename_index :old_events, 'index_events_on_project_id', 'index_old_events_on_project_id'
-    rename_index :old_events, 'index_events_on_target_id', 'index_old_events_on_target_id'
-    rename_index :old_events, 'index_events_on_target_type', 'index_old_events_on_target_type'
+
+    if index_name_exists?(:old_events, 'index_events_on_action', false)
+      rename_index :old_events, 'index_events_on_action', 'index_old_events_on_action'
+    end
+
+    if index_name_exists?(:old_events, 'index_events_on_author_id', false)
+      rename_index :old_events, 'index_events_on_author_id', 'index_old_events_on_author_id'
+    end
+
+    if index_name_exists?(:old_events, 'index_events_on_created_at', false)
+      rename_index :old_events, 'index_events_on_created_at', 'index_old_events_on_created_at'
+    end
+
+    if index_name_exists?(:old_events, 'index_events_on_project_id', false)
+      rename_index :old_events, 'index_events_on_project_id', 'index_old_events_on_project_id'
+    end
+
+    if index_name_exists?(:old_events, 'index_events_on_target_id', false)
+      rename_index :old_events, 'index_events_on_target_id', 'index_old_events_on_target_id'
+    end
+
+    if index_name_exists?(:old_events, 'index_events_on_target_type', false)
+      rename_index :old_events, 'index_events_on_target_type', 'index_old_events_on_target_type'
+    end
+
   end
 end
