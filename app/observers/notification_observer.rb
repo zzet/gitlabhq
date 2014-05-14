90422a94dc323d1d1b73a2c1f4c99a32eafc42d4
Andrey Kumanyaev <me@zzet.org>
2014-05-14 12:36:29 +0400

    add to queue after AR commit

--- app/observers/notification_observer.rb
+++ app/observers/notification_observer.rb
@@ -1,9 +1,11 @@
 class NotificationObserver < ActiveRecord::Observer
   observe Event::Subscription::Notification
 
-  def after_create(notification)
-    unless notification.notification_state == :delayed
-      Sidekiq::Client.enqueue_to(:mail_notifications, MailNotificationWorker, notification.id)
+  def after_commit(notification)
+    if notification.send(:transaction_include_action?, :create)
+      unless notification.notification_state == :delayed
+        Sidekiq::Client.enqueue_to(:mail_notifications, MailNotificationWorker, notification.id)
+      end
     end
   end
 
