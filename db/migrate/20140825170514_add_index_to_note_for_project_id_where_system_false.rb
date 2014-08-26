3c58651666606879b324f810cad1d0a770a18188
Andrew Kumanyaev <me@zzet.org>
2014-08-26 13:30:37 +0400

    fix migration

--- db/migrate/20140825170514_add_index_to_note_for_project_id_where_system_false.rb
+++ db/migrate/20140825170514_add_index_to_note_for_project_id_where_system_false.rb
@@ -4,8 +4,6 @@ class AddIndexToNoteForProjectIdWhereSystemFalse < ActiveRecord::Migration
               name: 'not_system_notes',
               where: 'system = false')
 
-    remove_index(:events, name: 'for_main_dashboard_index')
-
     add_index(:events, [:target_type, :target_id,
                         :source_type, :source_id,
                         :first_domain_type, :first_domain_id,
@@ -17,12 +15,6 @@ class AddIndexToNoteForProjectIdWhereSystemFalse < ActiveRecord::Migration
 
   def down
     remove_index(:events, name: 'for_main_dashboard_index')
-
-    add_index(:events, [:target_type, :target_id, :first_domain_type,
-                        :first_domain_id, :second_domain_type, :second_domain_id],
-              name: 'for_main_dashboard_index',
-              order: {created_at: :desc},
-              where: 'parent_event_id is NULL')
 
     remove_index(:notes, name: 'not_system_notes')
   end
